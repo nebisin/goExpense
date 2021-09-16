477ae469ab36fab1ebede7987e95fa74755daf0d
nebisin <massive00@windowslive.com>
2021-09-16 03:33:51 +0300

    required authentication endpoints

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -29,10 +29,10 @@ func (s *server) setupRoutes() {
 
 	apiV1.HandleFunc("/users", s.requireAuthenticatedUser(s.handleUpdateUser)).Methods(http.MethodPatch)
 
-	apiV1.HandleFunc("/transactions", s.requireAuthenticatedUser(s.handleCreateTransaction)).Methods(http.MethodPost)
+	apiV1.HandleFunc("/transactions", s.requireActivatedUser(s.handleCreateTransaction)).Methods(http.MethodPost)
+	apiV1.HandleFunc("/transactions/{id:[0-9]+}", s.requireActivatedUser(s.handleDeleteTransaction)).Methods(http.MethodDelete)
+	apiV1.HandleFunc("/transactions/{id:[0-9]+}", s.requireActivatedUser(s.handleUpdateTransaction)).Methods(http.MethodPatch)
 	apiV1.HandleFunc("/transactions/{id:[0-9]+}", s.requireAuthenticatedUser(s.handleGetTransaction)).Methods(http.MethodGet)
-	apiV1.HandleFunc("/transactions/{id:[0-9]+}", s.requireAuthenticatedUser(s.handleDeleteTransaction)).Methods(http.MethodDelete)
-	apiV1.HandleFunc("/transactions/{id:[0-9]+}", s.requireAuthenticatedUser(s.handleUpdateTransaction)).Methods(http.MethodPatch)
 	apiV1.HandleFunc("/transactions", s.requireAuthenticatedUser(s.handleListTransactions)).Methods(http.MethodGet)
 }
 
