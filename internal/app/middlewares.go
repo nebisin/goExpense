d1db7a3830d77c7c347c439fdbd0d5bd6dc9c06d
nebisin <massive00@windowslive.com>
2021-09-19 16:55:59 +0300

    Panic Recovery

--- internal/app/middlewares.go
+++ internal/app/middlewares.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"errors"
+	"fmt"
 	"net"
 	"net/http"
 	"strings"
@@ -143,6 +144,19 @@ func (s *server) rateLimit(next http.Handler) http.Handler {
 		}
 
 		s.limiter.mu.Unlock()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (s *server) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				response.ServerErrorResponse(w, r, s.logger, fmt.Errorf("%s", err))
+			}
+		}()
 
 		next.ServeHTTP(w, r)
 	})
