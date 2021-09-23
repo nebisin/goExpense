f16ece7f9639305ebecf3dcaf43918cbd8a15a7f
Salim Bozok <salimbozok@outlook.com>
2021-09-23 18:48:55 +0300

    increase test coverage

--- internal/store/user_store_test.go
+++ internal/store/user_store_test.go
@@ -91,3 +91,47 @@ func TestGetUserByEmail(t *testing.T) {
 		require.Empty(t, user2)
 	})
 }
+
+func TestUpdateUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	t.Run("success case for update user", func(t *testing.T) {
+		user2 := user1
+		user2.IsActivated = true
+
+		err := testModels.Users.Update(&user2)
+		require.NoError(t, err)
+		require.NotEmpty(t, user2)
+
+		require.Equal(t, user1.Name, user2.Name)
+		require.Equal(t, user1.Email, user2.Email)
+		require.Equal(t, user1.Password.Hashed, user2.Password.Hashed)
+		require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+		require.NotEqual(t, user1.IsActivated, user2.IsActivated)
+		require.Equal(t, user1.Version+1, user2.Version)
+	})
+
+	t.Run("duplicate email case for update user", func(t *testing.T) {
+		user3 := createRandomUser(t)
+
+		user2 := user1
+		user2.IsActivated = true
+		user2.Version = user1.Version + 1
+		user2.Email = user3.Email
+
+		err := testModels.Users.Update(&user2)
+		require.Error(t, err)
+		require.ErrorIs(t, err, store.ErrDuplicateEmail)
+	})
+
+	t.Run("edit conflict case for update user", func(t *testing.T) {
+		user2 := user1
+		user2.IsActivated = true
+
+		err := testModels.Users.Update(&user2)
+		require.Error(t, err)
+		require.ErrorIs(t, err, store.ErrEditConflict)
+	})
+
+}
