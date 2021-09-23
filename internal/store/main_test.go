09049a35d6fb4596820a6072821eb7db406196c5
Salim Bozok <salimbozok@outlook.com>
2021-09-23 17:57:06 +0300

    main test function

---
+++ internal/store/main_test.go
@@ -0,0 +1,30 @@
+package store_test
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/nebisin/goExpense/internal/store"
+)
+
+var testModels *store.Models
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := store.OpenDB(os.Getenv("DB_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testDB = db
+	testModels = store.NewModels(db)
+
+	os.Exit(m.Run())
+}
