1a7dbc906f9544c68d5720110e93caae2f3d16d4
Salim Bozok <salimbozok@outlook.com>
2021-10-05 11:22:03 +0300

    small fix

--- internal/store/transaction_store.go
+++ internal/store/transaction_store.go
@@ -136,8 +136,8 @@ func (m *transactionModel) GetAll(userId int64, title string, tags []string, sta
 	query := fmt.Sprintf(`SELECT id, user_id, account_id, type, title, description, tags, amount, payday, created_at, version
 	FROM transactions
 	WHERE user_id = $1 
-	AND (to_tsvector('simple', title) @@ plainto_tsquery('simple', $2) OR $2='')
-	OR (tags @> $7 OR $7 = '{}')
+	AND ((to_tsvector('simple', title) @@ plainto_tsquery('simple', $2) OR $2='')
+	OR (tags @> $7 OR $7 = '{}'))
 	AND payday >= $3 AND payday < $4
 	ORDER BY %s %s, id ASC
 	LIMIT $5 OFFSET $6`, filters.sortColumn(), filters.sortDirection())
