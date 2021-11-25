96e7dfcfcbf021c450c695dfadfbd37530f6264a
Salim Bozok <salimbozok@outlook.com>
2021-11-25 19:41:58 +0300

    add user to an account and get account users endpoints

---
+++ internal/store/account_tx.go
@@ -0,0 +1,36 @@
+package store
+
+import (
+	"context"
+	"time"
+)
+
+func (m *Models) CreateAccountTX(account *Account) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	tx, err := m.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	txModels := NewModelsWithTX(tx)
+
+	err = txModels.Accounts.Insert(account)
+	if err != nil {
+		return err
+	}
+
+	err = txModels.Accounts.AddUser(account.ID, account.OwnerID)
+	if err != nil {
+		return err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+
+}
