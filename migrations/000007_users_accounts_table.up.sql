935e5a5d1a04f4bdf6a0bc6275fabfecdec7c249
nebisin <massive00@windowslive.com>
2021-11-27 03:01:13 +0300

    initially deferred constraints and docs

--- migrations/000007_users_accounts_table.up.sql
+++ migrations/000007_users_accounts_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users_accounts (
     user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
-    account_id bigint NOT NULL REFERENCES accounts ON DELETE CASCADE,
+    account_id bigint NOT NULL REFERENCES accounts ON DELETE CASCADE INITIALLY DEFERRED,
     PRIMARY KEY (user_id, account_id)
 );
